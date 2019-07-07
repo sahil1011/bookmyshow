66bc4eeb722eec36afe61c06d5286608e79f119c
sahil1011 <sahilsa71@gmail.com>
2019-07-07 10:35:24 +0530

    error removal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bms'`, () => {
+  it(`should have as title 'bookmyshow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bms');
+    expect(app.title).toEqual('bookmyshow');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bms!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bookmyshow!');
   });
 });
